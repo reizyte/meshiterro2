892e7c4c26e1409052701732535ba5016efd87a3
reizyte <112692614+reizyte@users.noreply.github.com>
2022-12-22 14:48:38 +0000

    [finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,13 +9,19 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image = PostImage(params[:id])
+    @post_image = PostImage.find(params[:id])
   end
 
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
+    redirect_to post_images_path
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
     redirect_to post_images_path
   end
 
